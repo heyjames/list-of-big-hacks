6ef1af2460e35025627e356e7670b80e0febb409
James <heyjames@users.noreply.github.com>
2017-11-03 00:10:59 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,139 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="google-site-verification" content="N55N43TKuKNK1nKMNTDD8YVsXwQFSlNjCE2fHj3GjfM" />
+    <title>List of Big Hacks</title>
+    <style>
+      body {
+        background-color: #FAFAFA;
+        font-family: verdana, helvetica, arial;
+        margin: 0;
+      }
+      
+      h1, h5, .footer {
+        text-align: center;
+      }
+      
+      h1 {
+        margin: 0 0 0 0;
+      }
+      
+      h5 {
+        font-weight: normal;
+        margin: 0 0 15px 0;
+      }
+      
+      hr {
+         height:1px;
+         border-width:0;
+         width: 600px;
+         background-color: lightgray;
+      }
+      
+      #wrapper {
+        background-color: #FFFFFF;
+        max-width: 780px;
+        margin:0 auto 0 auto;
+        box-shadow: 0px 0px 10px #C0C0C0;
+      }
+      
+      .table {
+        margin: 0px auto;
+      }
+      
+      .footer {
+        font-size: 0.8em;
+        color: #bcbcbc;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        background-color: #444444;
+      }
+      
+      .table tbody td {
+        padding-left: 12px;
+        padding-right: 12px;
+      }
+      
+      .table tr {
+        height: 26px;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="wrapper">
+      <h1>List of Big Hacks</h1><h5>Major events of data breach</h5><hr>      
+      <table class="table">
+        <tbody>
+          
+          <tr>
+<td>2015-10</td>
+<td><a target="_blank" href="https://www.bostonglobe.com/business/2015/10/01/experian-says-info-from-million-mobile-customers-hacked/9zRR8wRiKP8G4b3ksC8iRJ/story.html">Experian</a></td>
+<td>15 million T-Mobile customers affected</td>
+</tr>
+<tr>
+<td>2015-10</td>
+<td><a target="_blank" href="http://money.cnn.com/2015/10/02/technology/scottrrade-hack">Scottrade</a></td>
+<td>4.6 million customers affected</td>
+</tr>
+<tr>
+<td>2015-07</td>
+<td><a target="_blank" href="http://krebsonsecurity.com/2015/07/online-cheating-site-ashleymadison-hacked/">Ashley Madison</a></td>
+<td>37 million customers affected</td>
+</tr>
+<tr>
+<td>2015-06</td>
+<td><a target="_blank" href="http://www.independent.co.uk/news/world/americas/opm-says-56-million-fingerprints-stolen-in-biggest-cyber-attack-in-us-history-10515256.html">U.S. Office of Personnel Mgt.</a></td>
+<td>21 million employees affected</td>
+</tr>
+<tr>
+<td>2015-02</td>
+<td><a target="_blank" href="http://money.cnn.com/2015/02/04/technology/anthem-insurance-hack-data-security/">Anthem Inc</a></td>
+<td>80 million customers affected</td>
+</tr>
+<tr>
+<td>2014-11</td>
+<td><a target="_blank" href="http://www.vox.com/2014/12/14/7387945/sony-hack-explained">Sony Pictures</a></td>
+<td>Sony Pictures's Corporate Network</td>
+</tr>
+<tr>
+<td>2014-09</td>
+<td><a target="_blank" href="http://money.cnn.com/2014/09/18/technology/security/home-depot-hack/">Home Depot</a></td>
+<td>56 million customers affected</td>
+</tr>
+<tr>
+<td>2014-08</td>
+<td><a target="_blank" href="http://time.com/3246562/hackers-jennifer-lawrence-cloud-data/">iCloud</a></td>
+<td>100+ celebrities affected</td>
+</tr>
+<tr>
+<td>2013-12</td>
+<td><a target="_blank" href="http://finance.yahoo.com/news/target-breach-affected-millions-more-184807005.html">Target</a></td>
+<td>110 million customers affected</td>
+</tr>
+<tr>
+<td>2013-10</td>
+<td><a target="_blank" href="http://arstechnica.com/security/2013/11/how-an-epic-blunder-by-adobe-could-strengthen-hand-of-password-crackers/">Adobe</a></td>
+<td>130 million customers affected</td>
+</tr>
+<tr>
+<td>2013-06</td>
+<td><a target="_blank" href="http://www.theguardian.com/world/2013/jun/06/nsa-phone-records-verizon-court-order">National Security Agency</a></td>
+<td></td>
+</tr>
+<tr>
+<td>2011-04</td>
+<td><a target="_blank" href="http://www.bbc.com/news/technology-13192359">PlayStation Network</a></td>
+<td>77 million customers affected</td>
+</tr>
+        </tbody>
+
+      </table>
+      <br>
+    <footer class="footer">
+      www.listofbighacks.com &copy; 2015 <!-- | @gmail.com -->
+    </footer>
+    </div> <!--wrapper-->
+  </body>
+</html>
